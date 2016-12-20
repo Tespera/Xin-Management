27f50809de10d0b9708fb212f9a74c29d87f2919
lewis-geek <lewis_geek@icloud.com>
2016-12-20 17:40:24 +0800

    day1

--- src/route.js
+++ src/route.js
@@ -1,5 +1,19 @@
+import Main from './views/main.vue'
+
+import Test from './views/main/test.vue'
+
 const routes = [
-  
+  {
+    path: '/',
+    component: Main,
+    children: [
+      {
+        path: '/test',
+        name: 'test',
+        component: Test
+      }
+    ]
+  }
 ]
 
 export default routes
---
+++ src/views/main.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <div>
+    <el-row>
+      <el-col
+        :span='4'>
+        <nav class="left-nav">
+
+        </nav>
+      </el-col>
+      <el-col
+        :span='20'>
+        <main class="right-main">
+
+        </main>
+      </el-col>
+    </el-row>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+.left-nav {
+  background-color: red;
+  height: 30px;
+}
+
+.right-main {
+  background-color: blue;
+  height: 30px;
+}
+</style>
